00d8c89b95fd6d1cc59b237e6916e95a8487197e
이희수 <hslee9234@naver.com>
2024-05-17 16:09:46 +0900

    first commit

---
+++ src/main/java/com/example/momentgallery/MomentGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.example.momentgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentGalleryApplication.class, args);
+	}
+
+}
